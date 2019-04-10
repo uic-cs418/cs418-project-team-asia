bd334ed47473440f85d675b7a2a78890e4acf3b9
ryousu2 <47653210+ryousu2@users.noreply.github.com>
2019-04-10 02:28:38 -0500

    Add files via upload

---
+++ ConvertLocation.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import pandas as pd
+from arcgis.gis import GIS
+from arcgis.geocoding import reverse_geocode
+from arcgis.geometry import Geometry
+import math
+
+pd.options.mode.chained_assignment = None
+
+def FindAdress(row):
+	if (math.isnan(row['X Coordinate'])) or (math.isnan(row['Y Coordinate'])):
+		return None,None,None
+	else: 
+		pt = Geometry({"x": row['X Coordinate'],"y": row['Y Coordinate'],"spatialReference": {"wkid": 3435}})
+		result = reverse_geocode(pt)
+		
+		return pd.Series({
+			'Neighborhood': result['address']['Neighborhood'],
+			'Zipcode': result['address']['Postal'],
+			'Address': result})
+
+
+
+
+
+file = 'crimes.csv'
+
+data = pd.read_csv(file)
+
+testData = data[200011:300000]#lower and upper bound of rows your are converting
+
+
+gis = GIS("https://univofillinois.maps.arcgis.com/home/index.html", "id", "password")
+
+
+newcolumns = testData.apply(FindAdress,axis=1)
+	
+testData['Neighborhood'] = newcolumns['Neighborhood']
+testData['Zipcode'] = newcolumns['Zipcode']
+testData['Address'] = newcolumns['Address']
+
+print(testData.shape)
+
+testData.to_csv('200011To300000.csv')#add name of outputfile. Nameformat: LowerBoundToUpperbound.csv. For example: 0To99.csv
+
+
